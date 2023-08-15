7578cf16c39ce3ba6d589544d66a92e03016101d
Russell Anderson <rga@us.ibm.com>
2023-08-15 08:39:20 -0400

    forms, user accounts, and static files

--- blog/models.py
+++ blog/models.py
@@ -9,8 +9,8 @@ class Post(models.Model):
 	)
 	body  = models.TextField()
 
-def __str__(self):
-	return self.title
+	def __str__(self):
+		return self.title
 
-def get_absolute_url(self):
-	return reverse("post_detail", kwargs={"pk": self.pk})
+	def get_absolute_url(self):
+		return reverse("post_detail", kwargs={"pk": self.pk})
--- django_project/settings.py
+++ django_project/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "blog",
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -118,6 +121,8 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
